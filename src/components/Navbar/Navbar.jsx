b2e080730a958622503adcd86d7a8c333ce7c5ac
karmveer <karmveersingh2003@gmail.com>
2024-10-08 21:10:09 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../../assets/slogo.png";
+import logo from "../../assets/slogo.jpg";
 
 import { Link } from "react-router-dom";
 
